32990b7e309d37301dcebf628fecb27ccc785eb8
luis <araujodasilvaluisgustavo@gmail.com>
2022-11-29 22:41:57 -0300

    Copa

---
+++ app/src/main/java/com/has/copa/Jogadores.kt
@@ -0,0 +1,12 @@
+import com.has.copa.R
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Jogadores : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_jogadores)
+    }
+}
